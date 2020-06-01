a57940bba943f38e00fdf39c0215a4fde44da1a5
Arjun Khanduri <arjun99.khanduri@gmail.com>
2020-06-01 16:51:22 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 var express=require('express');
 var app=express();
 var bodyParser=require('body-parser');
+var fs=require('fs');
+var multer=require('multer');
 var mongoose=require('mongoose');
+var filename=Date.now()+'.png';
+var storage=multer.diskStorage({
+    destination: function(req,file,cb){
+        var path='./public/cards/'
+        cb(null,path);
+    },
+    filename: function(req,file,cb){
+        cb(null,filename);
+    }
+});
+var upload=multer({storage: storage});
+app.use(express.static('public'));
 app.set('view engine','ejs');
-mongoose.connect("mongodb://localhost:27017/stackhack-reg",{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:false});
 app.use(bodyParser.urlencoded({extended:true}));
+mongoose.connect("mongodb://localhost:27017/stackhack-reg",{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:false});
 
 var detSchema=new mongoose.Schema({
     fname: String,
@@ -12,25 +26,74 @@ var detSchema=new mongoose.Schema({
     num: String,
     email: String,
     type: String,
-    ticketn:String
+    ticketn:String,
+    created:{type: Date, default: Date.now()},
+    img:{data: Buffer, contentType: String}
 });
-var det=mongoose.model("det",detSchema);
+var det=mongoose.model('det',detSchema);
+
+
+//det.remove({},function(err){
+//    if(err)
+//        console.log(err);
+//    else
+//        console.log("Removed");
+//});
+
+
 
 app.get('/',function(req,res){
-    res.render('home'); 
-//    res.redirect('/confirm');
+    res.render('home',{detail:null}); 
 });
-app.post('/',function(req,res){
-   det.create(req.body.detail,function(err,newUser){
-      if(err)
-          console.log(err);
+app.post('/',upload.single('detail[img]'),function(req,res){
+    det.create(req.body.detail,function(err,newUser){
+        var id=newUser._id.toString();
+        var oldPath=__dirname+'/public/cards/'+filename;
+        var newPath=__dirname+'/public/cards/'+id+'.png';
+        fs.rename(oldPath,newPath,function(err){
+            if(err)
+                console.log(err);
+        });
+        if(err)
+            console.log(err);
+        else
+            res.redirect('/confirm/'+id);
+    });
+});
+app.get('/confirm/:id',function(req,res){
+    det.findById(req.params.id,function(err,foundDetail){
+        if(err)
+            console.log(err);
+        else
+            res.render('confirm',{detail:foundDetail});  
+    })
+
+});
+app.get('/success/:id',function(req,res){
+   det.findById(req.params.id,function(err,found){
+        if(err)
+            console.log(err);
+        else
+            res.render('success',{detail:found});
+   });
+});
+app.get('/edit/:id',function(req,res){
+    var path=__dirname+'/public/cards/'+req.params.id+'.png';
+    fs.unlink(path,function(err){
+        if(err)
+            console.log(err);
+    });
+    det.findById(req.params.id,function(err,found){
+       if(err)
+           console.log(err);
        else
-           res.redirect("/");
-   }); 
+           res.render('edithome',{detail:found});
+   });
+    det.remove({_id:req.params.id},function(err){
+        if(err)
+            console.log(err);
+    });
 });
-app.get("/confirm",function(req,res){
-    res.render('confirm');
-})
 app.listen(3000,function(req,res){
-    console.log("Server online on port number 3000");
+    console.log("Server listening on port 3000");
 });
